69301be43d502b0c47d36698c9e3c68d57894267
Mads Wulff Nielsen <mwn@Madss-MacBook-Pro.local>
2019-04-29 11:38:49 +0200

    finish

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Welcome from './Welcome';
 import Button from './Touchable';
 import Fetch from './Fetch';
--- Fetch.js
+++ Fetch.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Text, FlatList, View, Alert } from 'react-native';
+import { Text, FlatList, View, StyleSheet, ActivityIndicator } from 'react-native';
 
 const URL = "https://www.madsjustesen.dk/jwtbackend/api/info/apis"
 
 export default class Fetch extends React.Component {
-    state = { data: []};
+    state = { data: [], spinner: true };
 
     async componentDidMount() {
         try {
@@ -13,8 +13,8 @@ export default class Fetch extends React.Component {
                 person.key = person.name;
                 return person
             })
-            
-            this.setState({ data: data});
+
+            this.setState({ data: data, spinner: false });
         } catch (error) {
             console.log(error);
         }
@@ -23,11 +23,37 @@ export default class Fetch extends React.Component {
     render() {
         return (
             <View>
-            <FlatList
-                data={this.state.data}
-                renderItem={({ item }) => <Text>{item.name}</Text>}
-            />
+                {this.state.spinner ?
+                    <View style={styles.spinner}>
+                    <ActivityIndicator size="large" color="grey"/>
+                    </View>
+                    :
+                    <View>
+                        <FlatList
+                            data={this.state.data}
+                            renderItem={({ item }) => <Text>{item.name}</Text>}
+                        />
+                    </View>
+                }
             </View>
+
         )
     }
 }
+
+// Spinner = (props) => {
+//     return (
+//         <View style={[styles.container, styles.horizontal]}>
+//             <
+//         </View>
+//     )
+// }
+
+const styles = StyleSheet.create({
+    spinner: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingTop: 250,
+        padding: 8,
+    }
+})
